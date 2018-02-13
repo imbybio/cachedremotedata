45ce49f3656306a38a4c652dc17e4546d73b1719
Bruno Girin <bruno@imby.bio>
2018-02-13 16:05:58 +0000

    Added more constructor tests and getter tests

--- tests/CachedRemoteDataTest.elm
+++ tests/CachedRemoteDataTest.elm
@@ -17,6 +17,21 @@ constructors =
                     Expect.equal
                         NotAsked
                         (fromRemoteData RemoteData.NotAsked)
+            , test "Loading" <|
+                \() ->
+                    Expect.equal
+                        Loading
+                        (fromRemoteData RemoteData.Loading)
+            , test "Success v" <|
+                \() ->
+                    Expect.equal
+                        (Success "ok")
+                        (fromRemoteData (RemoteData.Success "ok"))
+            , test "Failure e" <|
+                \() ->
+                    Expect.equal
+                        (Failure "err")
+                        (fromRemoteData (RemoteData.Failure "err"))
             ]
         , describe "fromValue"
             [ test "Nothing" <|
@@ -41,5 +56,81 @@ constructors =
                     Expect.equal
                         (Failure "err")
                         (fromResult (Result.Err "err"))
+            ]
+        ]
+
+getters : Test
+getters =
+    describe "Functions that return partial data"
+        [ describe "value"
+            [ test "NotAsked" <|
+                \() ->
+                    Expect.equal
+                        Nothing
+                        (value NotAsked)
+            , test "Success v" <|
+                \() ->
+                    Expect.equal
+                        (Just "ok")
+                        (value (Success "ok"))
+            , test "Failure e" <|
+                \() ->
+                    Expect.equal
+                        Nothing
+                        (value (Failure "err"))
+            , test "Refreshing v" <|
+                \() ->
+                    Expect.equal
+                        (Just "ok")
+                        (value (Refreshing "ok"))
+            , test "Stale e v" <|
+                \() ->
+                    Expect.equal
+                        (Just "ok")
+                        (value (Stale "err" "ok"))
+            ]
+        , describe "result"
+            [ test "Loading" <|
+                \() ->
+                    Expect.equal
+                        Nothing
+                        (result Loading)
+            , test "Success v" <|
+                \() ->
+                    Expect.equal
+                        (Just (Result.Ok "ok"))
+                        (result (Success "ok"))
+            , test "Failure e" <|
+                \() ->
+                    Expect.equal
+                        (Just (Result.Err "err"))
+                        (result (Failure "err"))
+            , test "Refreshing v" <|
+                \() ->
+                    Expect.equal
+                        Nothing
+                        (result (Refreshing "ok"))
+            , test "Stale e v" <|
+                \() ->
+                    Expect.equal
+                        (Just (Result.Err "err"))
+                        (result (Stale "err" "ok"))
+            ]
+        , describe "remoteData"
+            [ test "Loading" <|
+                \() ->
+                    Expect.equal
+                        RemoteData.Loading
+                        (remoteData Loading)
+            , test "Refreshing v" <|
+                \() ->
+                    Expect.equal
+                        RemoteData.Loading
+                        (remoteData (Refreshing "ok"))
+            , test "Stale e v" <|
+                \() ->
+                    Expect.equal
+                        (RemoteData.Failure "err")
+                        (remoteData (Stale "err" "ok"))
             ]
         ]
